4e197531c83480a7a355d85732a402df4fc39294
Zacharie Tevaearai <zacharie.tevaearai@epfl.ch>
2022-11-04 15:37:16 +0100

    Switch to new nvim mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,40 +3,37 @@
 -- and plugins.
 --------------------------------------------------------
 
-local map = vim.api.nvim_set_keymap
+local vim_map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
 --Shortcut split navigation
-map('n', '<C-h>', '<C-w>h', {})
-map('n', '<C-j>', '<C-w>j', {})
-map('n', '<C-k>', '<C-w>k', {})
-map('n', '<C-l>', '<C-w>l', {})
-
-
---Remap for dealing with word wrap
--- map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
--- map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+vim_map('n', '<C-h>', '<C-w>h', {})
+vim_map('n', '<C-j>', '<C-w>j', {})
+vim_map('n', '<C-k>', '<C-w>k', {})
+vim_map('n', '<C-l>', '<C-w>l', {})
 
 --Add move line shortcuts
-map('n', '<A-j>', ':m .+1<CR>==', { noremap = true })
-map('n', '<A-k>', ':m .-2<CR>==', { noremap = true })
-map('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true })
-map('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true })
-map('v', '<A-j>', ':m \'>+1<CR>gv=gv', { noremap = true })
-map('v', '<A-k>', ':m \'<-2<CR>gv=gv', { noremap = true })
+vim_map('n', '<A-j>', ':m .+1<CR>==', { noremap = true })
+vim_map('n', '<A-k>', ':m .-2<CR>==', { noremap = true })
+vim_map('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true })
+vim_map('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true })
+vim_map('v', '<A-j>', ':m \'>+1<CR>gv=gv', { noremap = true })
+vim_map('v', '<A-k>', ':m \'<-2<CR>gv=gv', { noremap = true })
 
 -- nvim-tree
-map('n', '<C-n>', ':NvimTreeToggle<CR>', default_opts) -- open/close
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', default_opts) -- refresh
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', default_opts) -- search file
+vim_map('n', '<C-n>', ':NvimTreeToggle<CR>', default_opts) -- open/close
+vim_map('n', '<leader>r', ':NvimTreeRefresh<CR>', default_opts) -- refresh
+vim_map('n', '<leader>n', ':NvimTreeFindFile<CR>', default_opts) -- search file
 
 -- Telescope
-map('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
-map('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
+local telescope = require('telescope.builtin')
+map('n', '<leader><space>', telescope.buffers, default_opts)
+map('n', '<leader>sf', function() telescope.find_files({ previewer = false }) end, default_opts)
+map('n', '<leader>sb', telescope.current_buffer_fuzzy_find, default_opts)
+map('n', '<leader>sh', telescope.help_tags, default_opts)
+map('n', '<leader>st', telescope.tags, default_opts)
+map('n', '<leader>sd', telescope.grep_string, default_opts)
+map('n', '<leader>sp', telescope.live_grep, default_opts)
+map('n', '<leader>so', function() telescope.tags({ only_current_buffer = true }) end, default_opts)
+map('n', '<leader>?', telescope.oldfiles, default_opts)
